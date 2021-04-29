05fb7b04905455e9f10dd0d03db8883f0ce19b40
DeepaPadmanabhan <43021384+DeepaPadmanabhan@users.noreply.github.com>
2021-04-29 14:25:01 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+  
+}
+resource "aws_instance" "vminstanceaws-deepa" {
+  ami = "ami-0742b4e673072066f"
+  instance_type = "t2.micro"
+ 
+
+    tags = {
+      Name = "WEB-demo"
+    }
